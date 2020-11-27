5d44091342e1631a16d4a740b61ae15420f699c3
an-1024 <2550364626@qq.com>
2020-11-27 22:23:04 +0800

    添加坦克静止属性

--- src/tankAttributes/TankAttributes.java
+++ src/tankAttributes/TankAttributes.java
@@ -6,8 +6,13 @@ import java.awt.*;
 
 public class TankAttributes {
     private int x, y;
+    //初始方向
     private Direction dir = Direction.DOWN;
+    //移动速度
     private static final int SPEED=10;
+
+    //初始状态
+    private boolean moving = false;
 
 
     public TankAttributes(int x, int y, Direction dir) {
@@ -44,10 +49,25 @@ public class TankAttributes {
         return SPEED;
     }
 
+    public boolean isMoving() {
+        return moving;
+    }
+
+    public void setMoving(boolean moving) {
+        this.moving = moving;
+    }
+
     //将画笔交给tank自己，因为只有tank最知道自己的属性，而不是由我们
     //将属性取出来复制
     public void paint(Graphics g) {
         g.fillRect(x,y,50,50);
+        move();
+    }
+
+    private void move() {
+        if(!moving){
+            return;
+        }
         switch(dir){
             case LEFT:
                 x-=SPEED;
