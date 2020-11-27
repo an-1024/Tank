545c2ef217dd276c2e381bdebfacb9f62b47e2ae
an-1024 <2550364626@qq.com>
2020-11-27 19:24:48 +0800

    tank的属性封装

---
+++ src/tankAttributes/TankAttributes.java
@@ -0,0 +1,68 @@
+package tankAttributes;
+
+import newFrame.Direction;
+
+import java.awt.*;
+
+public class TankAttributes {
+    private int x, y;
+    private Direction dir = Direction.DOWN;
+    private static final int SPEED=10;
+
+
+    public TankAttributes(int x, int y, Direction dir) {
+        this.x = x;
+        this.y = y;
+        this.dir = dir;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public Direction getDir() {
+        return dir;
+    }
+
+    public void setDir(Direction dir) {
+        this.dir = dir;
+    }
+
+    public static int getSPEED() {
+        return SPEED;
+    }
+
+    //将画笔交给tank自己，因为只有tank最知道自己的属性，而不是由我们
+    //将属性取出来复制
+    public void paint(Graphics g) {
+        g.fillRect(x,y,50,50);
+        switch(dir){
+            case LEFT:
+                x-=SPEED;
+                break;
+            case UP:
+                y-=SPEED;
+                break;
+            case RIGHT:
+                x+=SPEED;
+                break;
+            case DOWN:
+                y+=SPEED;
+                break;
+            default:
+                break;
+        }
+    }
+}
