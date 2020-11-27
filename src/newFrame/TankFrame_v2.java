e4ffcf308b2ae5fdb1f71999b9cff57f1d20689d
an-1024 <2550364626@qq.com>
2020-11-28 00:47:23 +0800

    解决闪烁问题

--- src/newFrame/TankFrame_v2.java
+++ src/newFrame/TankFrame_v2.java
@@ -13,9 +13,11 @@ public class TankFrame_v2 extends Frame {
     TankAttributes myTank = new TankAttributes(200,200, Direction.DOWN);
     BulletAttributes bullet = new BulletAttributes(300,300,Direction.DOWN);
 
+    static final int GAME_WIDTH = 800, GAME_HEIGHT = 600;
+
     public TankFrame_v2() throws HeadlessException {
         //设置窗体大小
-        setSize(800,600);
+        setSize(GAME_WIDTH,GAME_HEIGHT);
         //禁止改变大小
         setResizable(false);
         //设置窗体title
@@ -33,6 +35,24 @@ public class TankFrame_v2 extends Frame {
         //按键设置
         this.addKeyListener(new MyKeyListener());
 
+    }
+
+    //解决闪烁问题,先将图片在内存画好，然后一次性画到屏幕上，类似先缓存好，然后一次性输出
+    //即内存内容复制到显存
+    Image offScrenImage = null;
+    @Override
+    public void update(Graphics g) {
+        if(offScrenImage == null){
+            offScrenImage = this.createImage(GAME_WIDTH, GAME_HEIGHT);
+        }
+
+        Graphics goffScren = offScrenImage.getGraphics();
+        Color c = goffScren.getColor();
+        goffScren.setColor(Color.black);
+        goffScren.fillRect(0,0,GAME_WIDTH, GAME_HEIGHT);
+        goffScren.setColor(c);
+        paint(goffScren);
+        g.drawImage(offScrenImage, 0, 0, null);
     }
 
     //将画笔传给tank自己，让它自己主导
